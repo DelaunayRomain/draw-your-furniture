9a378fe39eacb19ecd2695d834b1f4182f925b1c
Romain <disenofranchute@gmail.com>
2023-03-20 13:49:43 -0300

    feat(color-logic): create store for colors

---
+++ src/store/modules/color.js
@@ -0,0 +1,24 @@
+export default {
+  state() {
+    return {
+      colors: {
+        red: { spanish: 'Rojo', css: 'rgb(178, 34, 34)' },
+        blue: { spanish: 'Azul', css: 'rgb(30, 144, 255)' },
+        yellow: { spanish: 'Amarillo', css: 'rgb(255, 215, 0)' },
+        green: { spanish: 'Verde', css: 'rgb(34, 139, 34)' },
+        pink: { spanish: 'Rosado', css: 'rgb(255, 105, 180)' },
+        white: { spanish: 'Blanco', css: 'rgb(255, 255, 255)' },
+        black: { spanish: 'Negro', css: 'rgb(0, 0, 0)' },
+        grey: { spanish: 'Gris', css: 'rgb(169, 169, 169)' },
+        brown: { spanish: 'Cafe', css: 'rgb(139, 69, 19)' },
+      },
+    };
+  },
+  mutations: {},
+  getters: {
+    color(state) {
+      return state.color;
+    },
+  },
+  actions: {},
+};
